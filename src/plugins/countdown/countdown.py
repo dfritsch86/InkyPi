c9f8c6780ad3ce8a727e1b02e2a6bb64a07ace7c
Daniel Fritsch <daniel.fritsch.86@gmail.com>
2025-05-25 10:42:54 +0200

    fix settings issue

--- src/plugins/countdown/countdown.py
+++ src/plugins/countdown/countdown.py
@@ -11,7 +11,6 @@ DEFAULT_TIMEZONE = "US/Eastern"
 class Countdown(BasePlugin):
     def generate_settings_template(self):
         template_params = super().generate_settings_template()
-        template_params['clock_faces'] = CLOCK_FACES
         return template_params
 
     def generate_image(self, settings, device_config):
